5292c92776d13710a7786982eb0751ef6050c1d2
Emanuel Gustavo Vierne <emanuelgustavovierne@gmail.com>
2020-07-07 13:08:46 -0300

    add reactstrap

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,30 @@
 import React from "react";
+import {
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  Button
+} from "reactstrap";
 
-const Card = () => {
+const ShopCard = props => {
   return (
-    <div>
-      <h3>Card title</h3>
-    </div>
+    <Card>
+      <CardImg
+        width="120px"
+        src="https://d2skuhm0vrry40.cloudfront.net/2019/articles/2019-06-05-18-29/pokemon-sword-shield-new-pokemon-regional-pokedex-6017-1559755742879.jpg/EG11/thumbnail/750x422/format/jpg/quality/60"
+      />
+      <CardBody>
+        <CardTitle>Pokemon Name</CardTitle>
+        <CardSubtitle>Pokemon class</CardSubtitle>
+        <CardSubtitle>Valor:</CardSubtitle>
+        <CardText>$150,00</CardText>
+        <Button>Add to chart</Button>
+      </CardBody>
+    </Card>
   );
 };
 
-export default Card;
+export default ShopCard;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,19 @@
 import React from "react";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
 
 import "./style.css";
 
 const Header = () => {
   return (
     <div>
-      <nav className="nav-main">
-        <h1>Pokestore</h1>
-        <ul>
-          <li>Home</li>
-        </ul>
-        <input type="text" />
-      </nav>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand>Pokestore</NavbarBrand>
+        <Nav>
+          <NavItem>
+            <NavLink>Home</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
     </div>
   );
 };
