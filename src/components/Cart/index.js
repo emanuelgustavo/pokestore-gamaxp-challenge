e8e3d962fe2112c487efba619a477f8e1a27eace
Emanuel Gustavo Vierne <emanuelgustavovierne@gmail.com>
2020-07-11 17:56:09 -0300

    adding cart

---
+++ src/components/Cart/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Table } from "reactstrap";
+
+const Cart = () => {
+  return (
+    <div>
+      <h2>Chart</h2>
+      <Table responsive>
+        <thead>
+          <tr>
+            <th>img</th>
+            <th>Nome</th>
+            <th>Unit.</th>
+            <th>Qtde</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+export default Cart;
